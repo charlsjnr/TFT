e5187ee2780bf4377a0a705bda4a5492d4584999
CharlsJnr <18422@burnside.school.nz>
2022-02-28 14:36:58 +1300

    Jinja endblock

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+#imports Flask to activate website
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#creates route for home page
 @app.route('/')
 def home():
-    return "<h1> Hello World! </h1>"
+    return render_template("home.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
