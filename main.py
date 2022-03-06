a27e2ec695d324e9d2a8b96efe979ed0c06ffcd6
CharlsJnr <18422@burnside.school.nz>
2022-03-07 10:17:24 +1300

    styling

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
+@app.route('/directory')
+def directory():
+    return render_template("directory.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
+
